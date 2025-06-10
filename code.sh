73e952078babca6150ca83a6311c2488b3afb7d4
mathcabral <69159396+mathcabral@users.noreply.github.com>
2025-06-09 22:13:02 -0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -15,7 +15,7 @@ flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install com.spotify.Client -y #estou na duvida se instalo o snap
 flatpak install com.valvesoftware.Steam -y
 
-#melhora shader
+#melhora shader - dando erro
 echo "export AMD_VULKAN_ICD=RADV MESA_SHADER_CACHE_MAX_SIZE=12G" >> "$home/.profile"
 
 sudo apt update
@@ -28,4 +28,4 @@ echo "verificar o comando fastfetch, se nao aparecer o lubuntu tem que criar o a
 mkdir -p ~/.config/fastfetch
 fastfetch --gen-config ~/.config/fastfetch/config.jsonc
 adicionar no arquivo logo antes de modules -> "logo": {"source": "lubuntu"},
-para usar sempre que abrir um terminal -> echo "fastfetch" >> "$home/.bashrc" - adicionar fastfetch no final"
+para usar sempre que abrir um terminal -> echo "fastfetch" >> "$home/.bashrc" - adicionar fastfetch no final" #mesmo erro do shader
