6932698c0677c356ea44da5c2264dc9c076c2f16
mathcabral <69159396+mathcabral@users.noreply.github.com>
2025-06-10 14:44:03 -0300

    Update code.sh

--- code.sh
+++ code.sh
@@ -7,7 +7,7 @@ sudo apt autoremove -y
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install intel-media-va-driver mesa-va-drivers mesa-utils alsa-base fastfetch htop picom picom-conf qalculate-qt usb-creator-kde gufw plasma-discover flatpak -y
+sudo apt install intel-media-va-driver mesa-va-drivers mesa-utils alsa-base fastfetch zram-config htop picom picom-conf qalculate-qt usb-creator-kde gufw plasma-discover flatpak -y
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install org.mozilla.firefox -y
